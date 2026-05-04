f37772d274ed9bb2e81d53ee9a672ec004225ebf
ILÁN URIEL BUSSO <48857364@ort>
2026-05-04 16:28:53 -0300

    avanzando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import './App.css'
-import { useState } from 'react'
-import { buscarPeliculas } from './services/movieApi'
-import Titulo from './components/titulo'
-import InputBusqueda from './components/inputBusqueda'
-import ListaCard from './components/listaCard'
-import Loader from './components/loader'
-import MensajeError from './components/mensajeError'
+
 
 function App() {
   // aca van los useState + funciones
